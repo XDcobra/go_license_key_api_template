4a4954cc1dee95180a3bd5fff7191d66538a4a52
XDcobra <xdcobra11@gmail.com>
2025-06-19 21:28:33 +0700

    Added prometheus basic auth via env variables

--- services/api_gateway/router/server.go
+++ services/api_gateway/router/server.go
@@ -4,6 +4,7 @@ import (
 	prom "github.com/XDcobra/go_license_key_api_template/prometheus"
 	"github.com/gofiber/fiber/v2"
 	"github.com/gofiber/fiber/v2/middleware/basicauth"
+	"os"
 )
 
 func CreateServer() *fiber.App {
@@ -12,7 +13,7 @@ func CreateServer() *fiber.App {
 	prometheus := prom.New("api_gateway")
 	prometheus.RegisterAt(app, "/metrics", basicauth.New(basicauth.Config{
 		Users: map[string]string{
-			"testuser": "testpw",
+			os.Getenv("METRICS_USER"): os.Getenv("METRICS_PASS"),
 		},
 	}))
 
