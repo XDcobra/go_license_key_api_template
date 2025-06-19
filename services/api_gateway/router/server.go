2ecc8b06f5fc50159ef1d34794f796d4a80d7bda
XDcobra <xdcobra11@gmail.com>
2025-06-19 20:00:34 +0700

    Added prometheus

--- services/api_gateway/router/server.go
+++ services/api_gateway/router/server.go
@@ -1,11 +1,22 @@
 package router
 
 import (
+	prom "github.com/XDcobra/go_license_key_api_template/prometheus"
 	"github.com/gofiber/fiber/v2"
+	"github.com/gofiber/fiber/v2/middleware/basicauth"
 )
 
 func CreateServer() *fiber.App {
 	app := fiber.New()
+
+	prometheus := prom.New("api_gateway")
+	prometheus.RegisterAt(app, "/metrics", basicauth.New(basicauth.Config{
+		Users: map[string]string{
+			"testuser": "testpw",
+		},
+	}))
+
+	app.Use(prometheus.Middleware)
 
 	return app
 }
